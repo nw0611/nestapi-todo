981887667b42b1270a3f40708dea60ade6098b46
NaokiWatanabe <nw@NaokiWatanabenoMacBook-Pro.local>
2024-04-23 16:33:30 +0900

    module controller service の雛形を作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, TodoModule],
   controllers: [AppController],
-  providers: [AppService],  // controller内で使いたいserviceをここでも指定する
+  providers: [AppService, PrismaService],  // controller内で使いたいserviceをここでも指定する
 })
 export class AppModule {}
